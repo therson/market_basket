192b6bfff10d399c4690d55ab4a08747beaa0fa3
Taylor Herson <therson@hortonworks.com>
2018-03-08 16:35:14 -0500

    changes to master.py

--- scripts/master.py
+++ scripts/master.py
@@ -10,32 +10,24 @@ class MarketBasket(Script):
     if not os.path.exists(params.install_dir): 
         os.makedirs(params.install_dir)
     os.chdir(params.install_dir)
-    Execute('yum install -y git')
-    Execute('git clone ' + params.download_url)
-    Execute('cd SE-demo/Retail/RetailAnalytics')
-    Execute('unzip OnlineRetail.txt.zip')
-    Execute('OnlineRetail.txt /tmp/')
-    Execute('sudo -u hdfs hadoop fs -mkdir /user/root')
-    Execute('sudo -u hdfs hadoop fs -mkdir /user/root/retail')
-    Execute('sudo -u hdfs hadoop fs -mkdir /user/root/retail/retailsalesraw')
-    Execute('sudo -u hdfs hadoop fs -chown -R root /user/root')
-    Execute('sudo -u hdfs hadoop fs -put /tmp/OnlineRetail.txt /user/root/retail/retailsalesraw')
+    Execute('params.install_dir + /CloudBreakArtifacts/recipes/market-basket-post-install.sh')
 
-
-  def pig(self, env):
+  def start(self, env):
     self.configure(env)
     import params
-    Execute('pig RetailSalesIngestion.pig')
-    Execute('pig MBADataPrep.pig')
+    Execute('touch /tmp/market_basket.pid')
 
-    
-  def hive(self, env):
+  def stop(self, env):
     self.configure(env)
     import params
-    Execute('cp RetailSalesRaw.ddl RetailSalesRaw.sql')
-    Execute('cp RetailSales.ddl RetailSales.sql')
-    Execute('hive -f RetailSalesRaw.sql')
-    Execute('hive -f RetailSales.sql')
+    Execute('rm -rf /tmp/market_basket.pid')
+
+  def status(self, env):
+    import params
+    env.set_params(params)
+    market_basket_pid_file = '/tmp/market_basket.pid'
+    Execute(format('cat {market_basket_pid_file}'))
+
 
   def configure(self, env):
     import params
